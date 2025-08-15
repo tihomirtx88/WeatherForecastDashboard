4550e5c7061ec03abd43b33b438a95448d906dfb
tihomirtx88 <tihomirtx88@gmail.com>
2025-08-15 11:48:10 +0300

    filtering forecast data

--- backend.py
+++ backend.py
@@ -9,7 +9,16 @@ def get_data(place, forecast_days=None, kind=None):
     url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={api_key}"
     response = requests.get(url);
     data = response.json();
-    return data
+    filtered_data = data["list"];
+    # filterenig days
+    nr_values = 8 * forecast_days;
+    filtered_data = filtered_data[:nr_values];
+
+    if kind == "Temperature":
+        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
+    if kind == "Sky":
+        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+    return filtered_data
 
 if __name__ == "__main__":
-    print(get_data(place="Sofia"));
+    print(get_data(place="Sofia", forecast_days=3, kind="Sky"));
