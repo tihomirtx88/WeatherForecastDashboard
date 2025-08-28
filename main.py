e9316b031102319b36343cadc453467110f54558
tihomirtx88 <tihomirtx88@gmail.com>
2025-08-28 15:54:36 +0300

    final fixing bugs

--- main.py
+++ main.py
@@ -15,15 +15,15 @@ if place:
         filtered_data = get_data(place, days);
 
         if option == "Temperature":
-            temperatures = [dict["main"]["temp"] / 10 for data in filtered_data];
-            dates = [dict["dt_txt"] for data in filtered_data];
+            temperatures = [data["main"]["temp"] / 10 for data in filtered_data];
+            dates = [data["dt_txt"] for data in filtered_data];
             # Create temperature plot
             figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperatures (C)"});
             st.plotly_chart(figure);
 
         if option == "Sky":
             images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png", "Rain": "images/rain.png", "Snow": "images/snow.png"};
-            sky_conditions = [dict["weather"][0]["main"] for data in filtered_data];
+            sky_conditions = [data["weather"][0]["main"] for data in filtered_data];
             image_path = [images[condition] for condition in sky_conditions];
             st.image(image_path, width=115);
     except KeyError:
